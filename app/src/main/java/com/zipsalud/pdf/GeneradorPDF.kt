2dbb24a04ffb36e43891dd387920873b15f202ea
Octaviano De La Torre <octavianodelatorre@gmail.com>
2018-09-08 21:44:25 -0500

    Corregir error en GeneradorPDF

--- app/src/main/java/com/zipsalud/pdf/GeneradorPDF.kt
+++ app/src/main/java/com/zipsalud/pdf/GeneradorPDF.kt
@@ -4,7 +4,6 @@
 
 package com.zipsalud.pdf
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
 import android.os.AsyncTask
@@ -14,18 +13,16 @@ import com.lowagie.text.*
 import com.lowagie.text.pdf.PdfWriter
 import harmony.java.awt.Color
 import com.zipsalud.R
-import com.zipsalud.modelo.IdUsuario
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
 
-class GeneradorPDF(@SuppressLint("StaticFieldLeak") val context:Context, var infPDF:InformacionPDF): AsyncTask<Boolean, Boolean, Boolean>() {
+class GeneradorPDF(val context:Context, var infPDF:InformacionPDF): AsyncTask<Boolean, Boolean, Boolean>() {
 
     companion object {
         private const val NOMBRE_DIRECTORIO = "ZipSaludPDF"
         private var NOMBRE_DOCUMENTO = "ZipSaludPdf.pdf"
-        private val idUsuario: String = IdUsuario.getInstance().idUsuario
     }
 
     override fun onPreExecute() {
@@ -34,13 +31,13 @@ class GeneradorPDF(@SuppressLint("StaticFieldLeak") val context:Context, var inf
     }
 
     override fun doInBackground(vararg p0: Boolean?): Boolean {
-        var resultado:Boolean = true
+        var resultado = true
         val documento = Document()
         val r: Resources = context.resources
 
         try {
             val file:File? = crearFichero(NOMBRE_DOCUMENTO)
-            val ficheroPdf: FileOutputStream = FileOutputStream(file?.getAbsolutePath())
+            val ficheroPdf = FileOutputStream(file?.absolutePath)
 
             var writer: PdfWriter = PdfWriter.getInstance(documento, ficheroPdf)
 
@@ -102,29 +99,29 @@ class GeneradorPDF(@SuppressLint("StaticFieldLeak") val context:Context, var inf
      * @return Paragraph
      */
     private fun crearTitulo(titulo:String? ): Paragraph {
-        val font: Font = FontFactory.getFont(FontFactory.HELVETICA, 14f, Font.BOLD, Color.DARK_GRAY);
+        val font: Font = FontFactory.getFont(FontFactory.HELVETICA, 14f, Font.BOLD, Color.DARK_GRAY)
         val t = Paragraph(titulo,font)
-        t.setAlignment(Element.ALIGN_CENTER)
+        t.alignment = Element.ALIGN_CENTER
         return t
     }
 
     /**
      * Crea un Subtitulo
      *
-     * @param Subtitle
+     * @param String
      * @return Paragraph
      */
     private fun crearSubTitulo(titulo:String? ): Paragraph {
-        val font: Font = FontFactory.getFont(FontFactory.HELVETICA, 12f, Font.BOLD, Color.DARK_GRAY);
+        val font: Font = FontFactory.getFont(FontFactory.HELVETICA, 12f, Font.BOLD, Color.DARK_GRAY)
         val t = Paragraph(titulo,font)
-        t.setAlignment(Element.ALIGN_LEFT)
+        t.alignment = Element.ALIGN_LEFT
         return t
     }
 
     /**
      * Crea un encabezado
      *
-     * @param title
+     * @param String
      * @return Phrase
      */
     private fun crearEncabezado(titulo:String ): Phrase {
@@ -156,7 +153,7 @@ class GeneradorPDF(@SuppressLint("StaticFieldLeak") val context:Context, var inf
      */
     private fun getRuta(): File? {
        var ruta: File? = null
-        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageDirectory())){
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
             ruta =  File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
                     NOMBRE_DIRECTORIO)
             if (!ruta.mkdirs()) {
